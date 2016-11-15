8270e7b74cf518f8deaf42702f38931da26537e2
denissimo83 <denissimo@opmbx.org>
2016-11-15 19:27:03 +1000

    Hello.py

---
+++ hello.py
@@ -0,0 +1,12 @@
+def application(environ, start_response):
+    query = environ[QUERY_STRING]
+    status = '200 OK'
+    headers = [('Content-Type', 'text/plain')]
+    result = []
+    if query is not None:
+        if query.find('&') >= 0:
+            result = query.split('&')
+        else result.append(query)
+    start_response(status, headers)
+    return result
+
