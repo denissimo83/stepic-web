7366e0c8be31321999277632cea7c837dd684fb3
denissimo83 <denissimo@opmbx.org>
2016-11-16 11:42:13 +1000

    Complete Lesson 1

--- hello.py
+++ hello.py
@@ -1,12 +1,13 @@
 def application(environ, start_response):
-    query = environ[QUERY_STRING]
+    query = environ['QUERY_STRING']
     status = '200 OK'
     headers = [('Content-Type', 'text/plain')]
     result = []
     if query is not None:
         if query.find('&') >= 0:
             result = [x + '\n' for x in query.split('&')]
-        else result.append(query + '\n')
+        else:
+            result.append(query + '\n')
     start_response(status, headers)
     return result
 
--- init.sh
+++ init.sh
@@ -2,5 +2,7 @@ sudo rm -f /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
+sudo /etc/init.d/gunicorn stop
+cd /home/box/web
+gunicorn -D -c ./etc/hello.py hello:application
 sudo /etc/init.d/mysql start
