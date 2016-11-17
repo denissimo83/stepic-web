fc638fb95dededf2ccf688e4591f389e507b403d
denissimo83 <denissimo@opmbx.org>
2016-11-17 17:04:42 +1000

    View 0.1

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,4 +1,30 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-def test(request, *args, **kwargs):
-    return HttpResponse('OK')
+from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_GET
+from django.core.paginator import Paginator
+#def test(request, *args, **kwargs):
+#    return HttpResponse('OK')
+
+def question_detail(request, id):
+    question = get_object_or_404(Question, id=id)
+    return render(request, 'qa/question_detail.html', {
+        'question': question,
+        'answers': question.answer_set.all()[:]
+        
+        })
+
+
+def question_list_all(request):
+    question = Question.objects.filter(
+    limit = request.GET.get('limit', 10)
+    page = request.GET.get('page', 1)
+    paginator = Paginator(question, limit)
+    paginator.baseurl = '/?page='
+    page = paginator.page(page)
+    return render(request, 'qa/question.html',
+        'post': page.object_list,
+        'paginator': paginator,
+        'page': page,
+    })
+
+
+
--- mysql.sh
+++ mysql.sh
@@ -1,4 +1,6 @@
 sudo /etc/init.d/mysqld start
+sudo /etc/init.d/mysql start
 sudo mysql -u root -e "CREATE DATABASE box_django CHARACTER SET utf8"
 sudo mysql -u root -e "CREATE USER 'box'@'localhost' IDENTIFIED BY '1234'"
-sudo mysql -u root -e "GRANT ALL PRIVELEGES ON box_django.* TO 'box'@'localost'"
+sudo mysql -u root -e "GRANT ALL PRIVILEGES ON box_django.* TO 'box'@'localost'"
+sudo mysql -u root -e "FLUSH PRIVILEGES"
